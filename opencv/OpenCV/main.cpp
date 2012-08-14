5d66fd261fbc028b4ca821bc2980570ca32ce00d
Kamil Zainal <akz08@imperial.ac.uk>
2012-08-14 17:33:37 +0100

    merged cleaned.h into main.h and introduced test bg subtraction

--- opencv/OpenCV/main.cpp
+++ opencv/OpenCV/main.cpp
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "cleaned.h"
 
 // Global variables
 VideoCapture capture;
@@ -22,8 +21,18 @@ int main()
     if(undistorted)
     {
         undistort(rawCameraMat,undistortedCameraMat, camMat, distortMat);
-        persp = initPerspective(false, undistortedCameraMat, CLICKRECTANGLE, H, dstSize);
+        persp = initPerspective(true, undistortedCameraMat, CLICKRECTANGLE, H, dstSize);
     }    
+    
+//    int i = 0;
+    
+    // trying to perform a gaussian bg-fg segmentation
+//    Mat frame, back, fore;
+//    BackgroundSubtractorMOG2 bg(100, 6, false);
+//    vector<vector<Point> > contours;
+
+    Mat frame, foreground;
+    BackgroundSubtractorMOG mog;
     
     while(true&&undistorted&&persp)
     {
@@ -34,8 +43,42 @@ int main()
         undistort(rawCameraMat, undistorted, camMat, distortMat);
         warpPerspective(undistorted, processed, H, dstSize,  INTER_LINEAR+CV_WARP_FILL_OUTLIERS, BORDER_CONSTANT, Scalar());
 
-        imshow("output", processed);
+//        processed.copyTo(frame);
+//        bg.operator()(frame, fore);
+//        bg.getBackgroundImage(back);
+//        erode(fore, fore, Mat::ones(Size(4,4), CV_64F));
+//        dilate(fore, fore, Mat::ones(Size(4,4), CV_64F) );   
+//        findContours(fore, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+//        drawContours(processed, contours, -1, Scalar(255,0,0));
+//        imshow("background", back);
+//        imshow("foreground", fore);
+        
+        cvtColor(processed, processed, CV_RGB2HSV);
+        processed.copyTo(frame);
+        mog(frame,foreground,0.001);
+        erode(foreground, foreground, Mat::ones(Size(4,4),CV_32F));
+        dilate(foreground, foreground, Mat::ones(Size(4,4),CV_32F));
+//        threshold(foreground, foreground, 155, 255, THRESH_BINARY_INV); // just to complement the image... can just do a NOT
+        bitwise_not(foreground, foreground);
+        imshow("gaussian", foreground);
+        
+        
+        
+//        cvtColor(processed, processed, CV_BGR2GRAY);
 
+        
+        
+//        if(i==0){
+//        processed.copyTo(bg); 
+//            i++;
+//        }
+
+        
+        
+        
+        imshow("output", processed);
+        
+//        createTrackbar("canny threshold", "output", &lowThreshold, 100);
         
         char key =  waitKey(100);
         if( key  == 27 ) // 27 == ESC
