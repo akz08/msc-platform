adc2df48b8b76620a292aa6576fc8d0d81a656b9
Kamil Zainal <akz08@imperial.ac.uk>
2012-08-19 18:16:30 +0100

    starting class implementation of functions

---
+++ OpenCV/vision/camHelper.h
@@ -0,0 +1,26 @@
+
+#ifndef msc_platform_camHelper_h
+#define msc_platform_camHelper_h
+
+class camHelper 
+{
+    private:
+    
+    
+    public:
+    
+    camHelper()
+    {
+        
+    }
+    
+    ~camHelper()
+    {
+        
+    }
+    
+    
+};
+
+
+#endif
